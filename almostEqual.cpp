50eb577c24d2d281f856e2bcd6874ed4a171e3f1
Marco Fiorito <marcofiorito1@hotmail.com>
2019-08-20 00:36:40 -0300

    functions auxiliars of almostEqual

--- almostEqual.cpp
+++ almostEqual.cpp
@@ -1,5 +1,5 @@
-/*
-Algorithm:
+/* Exercise from codeforce: https://codeforces.com/contest/1205/problem/A
+*Algorithm:
 - Generate every permutation of 2n numbers between 1 and 2n
 - For each of these permutation check if every sum of n numbers differ at most one.
 - If any return NO
@@ -46,13 +46,78 @@ int main()
 //It verifies if each sum of n elements differ in at most 1
 bool almostEqual(int[] arr, int n)
 {
-    //It missing to implement th9is algorithm
+    int currentIndex = 0;
+    int lastSum = -1;
+    int nextSum = 0;
+    int counter = 0;
+    for (int i = currentIndex; i < sizeof(arr); i++)
+    {
+        if (counter < n)
+        {
+            nextSum += arr[i];
+            counter++;
+        }
+        else
+        {
+            if (lastSum != -1 && fabs(nextSum - lastSum) > 1)
+            {
+                return false;
+            }
+            else
+            {
+                counter = 0;
+                i = ++currentIndex;
+                lastSum = nextSum;
+                nextSum = 0;
+            }
+        }
+    }
+
+    return true;
 }
 
 //It generates every permutations between 1 - maxNumber
 int[][] generatePermutations(int maxNumber)
 {
+    //Fill the basic array to then generate the permutations
+    int[] orderedArray = new int[maxNumber];
+    for (int i = 1; i <= maxNumber)
+    {
+        orderedArray[i - 1] = i;
+    }
+    return permute(orderedArray);
     //It missing to implement this algorithm
+}
+
+//algorithm found in: https://stackoverflow.com/questions/2920315/permutation-of-array
+int[][] permute(int[] nums)
+{
+    int[][] results = new int[][];
+    if (sizeof(nums) == 0)
+    {
+        return results;
+    }
+    int[] result = new ArrayList<>();
+    dfs(nums, results, result);
+    return results;
+}
+
+void dfs(int[] nums, int[][] results, int[] result)
+{
+    if (sizeof(nums) == sizeof(result))
+    {
+        int[] temp = result;
+        add(temp, results);
+    }
+    for (int i = 0; i < sizeof(nums); i++)
+    {
+        if (!contains(nums[i], result))
+        {
+            add(nums[i], result);
+            dfs(nums, results, result);
+            remove(result, sizeof(result) - 1);
+        }
+    }
 }
 
 //It prints the array in one line

324a8ac772764b700f5567c6b5b238b18e8b074f
Santiago <santiagotopolansky@gmail.com>
2019-08-21 07:23:00 -0300

    Version que compila Almost Equal
    Utiliza la funcion next_permutation de <algorithm>.
    Funciona correctamente.
    Tiene el problema de que para n > 6 crece exponencialmente el orden de ejecucion.
    Quizas se puede corregir algo para que corra en menor tiempo.

--- almostEqual.cpp
+++ almostEqual.cpp
@@ -1,130 +1,99 @@
 /* Exercise from codeforce: https://codeforces.com/contest/1205/problem/A
 *Algorithm:
-- Generate every permutation of 2n numbers between 1 and 2n
-- For each of these permutation check if every sum of n numbers differ at most one.
+- Generate array of 2n numbers between 1 and 2n
+- For each of its permutation check if every sum of n numbers differ at most one.
 - If any return NO
 - In first occurrece return Yes and this combination.
 */
-
+#define _CRT_SECURE_NO_WARNINGS
+#include <vector>
 #include <stdio.h>
 #include <string.h>
+#include <iostream>
+#include <algorithm> 
 
-int main()
-{
-    int n;
-    bool find = false;
-    int permToPrint[];
+using namespace std;
 
-    scanf("%d", &n);
-
-    int quantityOfPermutations = factorial(n * 2);
-
-    int[quantityOfPermutations][2 * n] everyPermutation = generatePermutations(2 * n);
-
-    for (int i = 0; i < quantityOfPermutations && !found; i++)
-    {
-        int[] currentPermutation = everyPermutation[i];
-
-        if (almostEqual(currentPermutation, n))
-        {
-            found = true;
-            permToPrint = currentPermutation;
-        }
-    }
-
-    if (found)
-    {
-        cout << "YES" << endl;
-        printArray(permToPrint);
-    }
-    else
-    {
-        cout << "NO" << endl;
-    }
-}
-
-//It verifies if each sum of n elements differ in at most 1
-bool almostEqual(int[] arr, int n)
-{
-    int currentIndex = 0;
-    int lastSum = -1;
-    int nextSum = 0;
-    int counter = 0;
-    for (int i = currentIndex; i < sizeof(arr); i++)
-    {
-        if (counter < n)
-        {
-            nextSum += arr[i];
-            counter++;
-        }
-        else
-        {
-            if (lastSum != -1 && fabs(nextSum - lastSum) > 1)
-            {
-                return false;
-            }
-            else
-            {
-                counter = 0;
-                i = ++currentIndex;
-                lastSum = nextSum;
-                nextSum = 0;
-            }
-        }
-    }
-
-    return true;
-}
-
-//It generates every permutations between 1 - maxNumber
-int[][] generatePermutations(int maxNumber)
-{
-    //Fill the basic array to then generate the permutations
-    int[] orderedArray = new int[maxNumber];
-    for (int i = 1; i <= maxNumber)
-    {
-        orderedArray[i - 1] = i;
-    }
-    return permute(orderedArray);
-    //It missing to implement this algorithm
-}
-
-//algorithm found in: https://stackoverflow.com/questions/2920315/permutation-of-array
-int[][] permute(int[] nums)
-{
-    int[][] results = new int[][];
-    if (sizeof(nums) == 0)
-    {
-        return results;
-    }
-    int[] result = new ArrayList<>();
-    dfs(nums, results, result);
-    return results;
-}
-
-void dfs(int[] nums, int[][] results, int[] result)
-{
-    if (sizeof(nums) == sizeof(result))
-    {
-        int[] temp = result;
-        add(temp, results);
-    }
-    for (int i = 0; i < sizeof(nums); i++)
-    {
-        if (!contains(nums[i], result))
-        {
-            add(nums[i], result);
-            dfs(nums, results, result);
-            remove(result, sizeof(result) - 1);
-        }
-    }
+int factorial(int n) {
+	int acum = 1;
+	while (n > 0) {
+		acum = acum * n;
+		n--;
+	}
+	return acum;
 }
 
 //It prints the array in one line
-void printArray(int[] arr)
+void printArray(int* arr, int n)
 {
-    for (int i; i < sizeOf(arr); i++)
-    {
-        cout << arr[i] << " " << end;
-    }
+	for (int i = 0; i < 2 * n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+
+bool almostEqual(int* currentArr, int n) {
+	int val1 = -1;
+	int sum2 = 0;
+	//First sum of n digits in the permutation.
+	for (int i = 0; i < n; i++) {
+		sum2 = sum2 + currentArr[i];
+	}
+	// All other sums of n consecutive digits compared one to another.
+	//If any difference between two of them is bigger than 1, it returns false.
+	for (int i = 0; i < 2 * n; i++) {
+		int sum1 = 0;
+		for (int j = 0; j < n; j++) {
+			if (i + j < 2 * n) {
+				sum1 = sum1 + currentArr[i + j];
+			}
+			else {
+				sum1 = sum1 + currentArr[i + j - 2 * n];
+			}
+		}
+		if (abs(sum2 - sum1) > 1) return false;
+		if (sum1 != sum2) {
+			if (val1 == -1) val1 = sum1;
+			else if (sum1 != val1) return false;
+		}
+	}
+	return true;
+}
+
+
+int main()
+{
+	freopen("outputAE.txt", "w", stdout);
+	int n;
+	scanf("%d", &n);
+	bool found = false;
+
+	//Initial order of number set
+	int* arrOriginal = new int[2 * n];
+	for (int i = 0; i < 2 * n; i++) {
+		arrOriginal[i] = i + 1;
+	}
+
+	//While there are more permutations, check if there is any almostEqual.
+	do {
+		if (almostEqual(arrOriginal, n))
+		{
+			found = true;
+		}
+	} while (!found && next_permutation(arrOriginal, arrOriginal + 2 * n));
+
+	if (found)
+	{
+		cout << "YES" << endl;
+		printArray(arrOriginal, n);
+	}
+	else
+	{
+		cout << "NO" << endl;
+	}
+	delete arrOriginal;
+
+	return 0;
 }
